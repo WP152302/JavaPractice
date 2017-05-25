9c737e0bdf4825386158a1f12afbfd2fbce8d1ef
권두영 <@DESKTOP-L3E30TO>
2017-05-25 11:38:25 +0900

    실습과제 13 제출

---
+++ JavaPractice/src/org/dimigo/interfaces/IDBManager.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package org.dimigo.interfaces;
+
+/**
+ * <pre>
+ * org.dimigo.interfaces
+ * 	|_ IDBManager
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 5. 25.
+ * </pre>
+ * 
+ * @author 권두영
+ * @version : 1.0
+ */
+public interface IDBManager {
+	public static final String ORACLE_DATABASE = "ORACLE";
+	public static final String SYBASE_DATABASE = "SYBASE";
+	public void insert();
+	public void search();
+	public void update();
+	public void delete();
+	public static IDBManager getDBObject(String db){
+		if(db.equals(ORACLE_DATABASE)){
+			return new OracleDB();
+		} else if (db.equals(SYBASE_DATABASE)){
+			return new SybaseDB();
+		}
+		return null;
+	}
+}
